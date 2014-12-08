6698b213a58461280fcb08324f9a128d2f01e66e
njarvis <npjarvis@uw.edu>
2014-12-07 22:11:43 -0800

    finished

--- js/shapes.js
+++ js/shapes.js
@@ -13,3 +13,58 @@
     or registerClassicalShape() to register your new shapes with the application. See the
     app.js file for info on these functions.
  */
+function Rectangle(left, top, width, height, stylesMap) {
+    Shape.call(this, left, top, width, height, stylesMap)
+    this.renderShape = function (canvasCtx) {
+        canvasCtx.fillRect(this.left, this.top, this.width, this.height);
+    }
+}
+
+Rectangle.prototype = new Shape();
+
+registerClassicalShape('Rectangle', Rectangle);
+
+function Circle(left, top, width, height, stylesMap) {
+    Shape.call(this, left, top, width, height, stylesMap);
+    this.renderShape = function(canvasCtx) {
+
+        canvasCtx.beginPath();
+
+        //horizontal coordinate for center
+        var a = this.left + width / 2;
+        //vertical coordinate for center
+        var b = this.top + height / 2;
+
+        //radius = half diameter
+        var rad = this.height / 2;
+
+        canvasCtx.arc(a, b, rad, 0, 2*Math.PI);
+
+        canvasCtx.fill();
+    }
+}
+
+Circle.prototype = new Shape();
+
+registerClassicalShape('Circle', Circle);
+
+
+function Custom(left, top, width, height, stylesMap) {
+    Shape.call(this, left, top, width, height, stylesMap);
+    this.renderShape = function(canvasCtx) {
+        canvasCtx.fillStyle = 'purple';
+        canvasCtx.beginPath();
+        canvasCtx.moveTo(this.top, this.left);
+        canvasCtx.lineTo(this.top + 25, this.left + 25);
+        canvasCtx.lineTo(this.top + 125, this.left + 100);
+        canvasCtx.lineTo(0, this.left + 120);
+        canvasCtx.closePath();
+        canvasCtx.fill();
+        canvasCtx.strokeText("Trying out some vertical text", this.height, this.top);
+        canvasCtx.strokeText("Trying out some horizontal text", this.left, this.height)
+    }
+
+}
+
+Custom.prototype = new Shape;
+registerClassicalShape('Custom', Custom);
